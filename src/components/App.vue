1ae07e4fd39e8669345fb95e2300406de6b8db12
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-02-05 15:23:57 +0100

    add .env

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="amount++">Click me</button>
+    <p>the amount of click are {{amount}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      amount : 0
+    }
+  },
+  methods:{
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import vue from "vue";
+import App from "./components/App";
+
+new Vue({
+  render: (h) => App,
+}).$mount("#app");
